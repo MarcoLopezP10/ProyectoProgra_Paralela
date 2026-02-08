a9a9717c3a759bfc9f92b2012e8351900a8fe298
MarcoLopezP10 <167968505+MarcoLopezP10@users.noreply.github.com>
2026-02-08 14:02:59 +0100

    Implement abstract Topology class and GlobalBestTopology

--- core/topology.py
+++ core/topology.py
@@ -1,1 +1,11 @@
+from abc import ABC,abstractmethod
 
+class Topology(ABC):
+    @abstractmethod
+    def get_best_position(self,particle,swarm):
+        pass
+
+class GlobalBestTopology(Topology):
+    def get_best_position(self, particle, swarm):
+        return swarm.global_best_position
+        
