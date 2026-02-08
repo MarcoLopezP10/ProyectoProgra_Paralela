7f9a228e5b28ccf5f5477bc19d550c9eb57fdbca
MarcoLopezP10 <167968505+MarcoLopezP10@users.noreply.github.com>
2026-02-08 13:01:17 +0100

    Add Particle class with velocity and position updates

--- core/particle.py
+++ core/particle.py
@@ -1,1 +1,27 @@
+import numpy as np
+
+class Particle:
+    def __init__(self,dim,bounds,rng): #Estado Propio, dimension, limites y generador aleatorio
+        self.position=rng.uniform(bounds[0],bounds[1],size=dim)
+        self.velocity=rng.uniform(-(bounds[1]-bounds[0]), (bounds[1]-bounds[0]),size=dim) 
+        self.best_position=self.position.copy()
+        self.best_fitness=np.inf
+    
+    def update_velocity(self,global_best_position,w,c1,c2,rng): #Actualizacion de velocidad(ecuacion PSO)
+        #Coeficientes aleatorios independientes
+        r1=rng.random(self.position.shape)
+        r2=rng.random(self.position.shape)
+
+        #Parte cognitiva y Social(enjambre)
+        cognitive=c1*r1*(self.best_position - self.position)
+        social=c2*r2*(global_best_position - self.position)
+
+        #Formula velocidad
+        self.velocity=w*self.velocity+cognitive+social
+    
+    def update_position(self):
+        self.position=self.position + self.velocity
+        
+
+
 
