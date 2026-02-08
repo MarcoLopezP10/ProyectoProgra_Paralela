fba2a286085fcfae682548be1aa1c2894d0d10fb
MarcoLopezP10 <167968505+MarcoLopezP10@users.noreply.github.com>
2026-02-08 14:03:38 +0100

    Add BoundsHandler and ClampBounds classes
    Implement BoundsHandler and ClampBounds classes for position clamping.

--- core/bounds.py
+++ core/bounds.py
@@ -1,1 +1,16 @@
+from abc import ABC,abstractmethod
+import numpy as np
 
+class BoundsHandler(ABC):
+    @abstractmethod
+    def apply(self,position,velocity):
+        pass
+
+class ClampBounds(BoundsHandler):
+    def __init__(self,bounds):
+        self.min,self.max=bounds
+    
+    def apply(self,position,velocity):
+        position =np.clip(position,self,min,self.max) #No tocamos la velocidad
+        return position,velocity 
+    
