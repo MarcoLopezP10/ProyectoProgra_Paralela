a49551ffb353d72fd98e374d44bddd0da880a2cb
MarcoLopezP10 <167968505+MarcoLopezP10@users.noreply.github.com>
2026-02-11 11:04:45 +0100

    Fix formatting and spacing in evaluator.py

--- core/evaluator.py
+++ core/evaluator.py
@@ -1,6 +1,13 @@
-from abc import ABC,abstractmethod
+from abc import ABC, abstractmethod
 
 class FitnessEvaluator(ABC):
     @abstractmethod
-    def evaluate(self,positions):
+    def evaluate(self, positions):
         pass
+
+class SequentialEvaluator(FitnessEvaluator):
+    def __init__(self, objective_fn):
+        self.objective_fn = objective_fn
+
+    def evaluate(self, positions):
+        return [self.objective_fn(x) for x in positions]
