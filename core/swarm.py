1a5b98fd927995fd5a98accd6ede8cc93051e8db
MarcoLopezP10 <167968505+MarcoLopezP10@users.noreply.github.com>
2026-02-12 12:42:04 +0100

    Fix indentation and add a newline in swarm.py

--- core/swarm.py
+++ core/swarm.py
@@ -1,6 +1,7 @@
 from .particle import Particle
 import numpy as np
 
+
 class Swarm:
     def __init__(self, n_particles, dim, bounds, rng):
         self.particles = [
@@ -14,6 +15,7 @@ class Swarm:
 
     def update_global_best(self, positions, fitness_values):
         for p, pos, fit in zip(self.particles, positions, fitness_values):
+
             if fit < p.best_fitness:
                 p.best_fitness = fit
                 p.best_position = pos.copy()
@@ -21,4 +23,3 @@ class Swarm:
             if fit < self.global_best_fitness:
                 self.global_best_fitness = fit
                 self.global_best_position = pos.copy()
-
