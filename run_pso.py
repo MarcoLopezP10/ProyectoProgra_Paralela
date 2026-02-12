6286f2cbba79657dab1e164a879d929782d59ff8
MarcoLopezP10 <167968505+MarcoLopezP10@users.noreply.github.com>
2026-02-12 12:44:24 +0100

    Update PSO to support multiple objectives and configurations
    Refactor PSO implementation to handle multiple objectives and adjust parameters.

--- run_pso.py
+++ run_pso.py
@@ -1,50 +1,60 @@
 import numpy as np
+
 from core.swarm import Swarm
 from core.pso import PSO
 from core.evaluator import SequentialEvaluator
 from core.bounds import ClampBounds
+from core.topology import GlobalBestTopology
+
 from objectives.sphere import sphere
-from objectives.ackely import ackley
+from objectives.ackley import ackley
 from objectives.rosenbrock import rosenbrock
-from objectives.rastringin import rastrigin
+from objectives.rastrigin import rastrigin
+
 
 def main():
     seed = 42
-    rng = np.random.default_rng(seed)
-
     dim = 2
     bounds = ([-5]*dim, [5]*dim)
 
-    swarm = Swarm(
-        n_particles=3000,
-        dim=dim,
-        bounds=bounds,
-        rng=rng
-    )
+    objectives = [sphere, ackley, rosenbrock, rastrigin]
 
-    
-    bounds_handler = ClampBounds(bounds[0], bounds[1])
+    for objective in objectives:
 
-    for objective in [sphere, ackley, rosenbrock, rastrigin]:
-        evaluator = SequentialEvaluator(objective)
-    
-        pso = PSO(
-        swarm=swarm,
-        evaluator=evaluator,
-        bounds_handler=bounds_handler,
-        w=0.7,
-        c1=1.5,
-        c2=1.5,
-        max_iters=100,
-        rng=rng
+        rng = np.random.default_rng(seed)
+
+        swarm = Swarm(
+            n_particles=500,
+            dim=dim,
+            bounds=bounds,
+            rng=rng
         )
 
-        best_pos, best_fit, elapsed = pso.run()
+        evaluator = SequentialEvaluator(objective)
+        bounds_handler = ClampBounds(bounds[0], bounds[1])
+        topology = GlobalBestTopology()
+
+        pso = PSO(
+            swarm=swarm,
+            evaluator=evaluator,
+            bounds_handler=bounds_handler,
+            topology=topology,
+            w=0.7,
+            c1=1.5,
+            c2=1.5,
+            max_iters=500,
+            tol=1e-10,
+            patience=30
+        )
+
+        best_pos, best_fit, elapsed, iters = pso.run()
 
         print("\nObjective:", objective.__name__)
         print("Best position:", best_pos)
         print("Best fitness:", best_fit)
+        print("Iterations:", iters)
         print("Elapsed time:", elapsed)
+
 
 if __name__ == "__main__":
     main()
