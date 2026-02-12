9257cfa3725002dc56e79094bfa1f41264d03c17
MarcoLopezP10 <167968505+MarcoLopezP10@users.noreply.github.com>
2026-02-12 12:41:13 +0100

    Refactor Particle class initialization and velocity update

--- core/particle.py
+++ core/particle.py
@@ -1,22 +1,32 @@
 import numpy as np
 
+
 class Particle:
     def __init__(self, dim, bounds, rng):
-        self.position = rng.uniform(bounds[0], bounds[1], size=dim)
-        self.velocity = rng.uniform(-1, 1, size=dim)
+        self.dim = dim
+        self.bounds = bounds
+        self.rng = rng
+
+        low, high = bounds
+
+        self.position = rng.uniform(low, high, dim)
+        self.velocity = rng.uniform(-(np.array(high) - np.array(low)),
+                                    (np.array(high) - np.array(low)))
+
         self.best_position = self.position.copy()
         self.best_fitness = np.inf
 
-    def update_velocity(self, gbest, w, c1, c2, rng):
-        r1 = rng.random(len(self.position))
-        r2 = rng.random(len(self.position))
+    def update_velocity(self, best_position, w, c1, c2):
+        r1 = self.rng.random(self.dim)
+        r2 = self.rng.random(self.dim)
+
         cognitive = c1 * r1 * (self.best_position - self.position)
-        social = c2 * r2 * (gbest - self.position)
+        social = c2 * r2 * (best_position - self.position)
+
         self.velocity = w * self.velocity + cognitive + social
 
     def update_position(self):
         self.position = self.position + self.velocity
-
 
 
 
