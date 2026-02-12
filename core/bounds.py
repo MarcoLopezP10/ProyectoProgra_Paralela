cdea697d74f04d80b0ac078a138ccd90a0fd9163
MarcoLopezP10 <167968505+MarcoLopezP10@users.noreply.github.com>
2026-02-12 12:40:26 +0100

    Refactor apply method to use np.clip

--- core/bounds.py
+++ core/bounds.py
@@ -1,14 +1,11 @@
 import numpy as np
 
+
 class ClampBounds:
     def __init__(self, lower, upper):
         self.lower = np.array(lower)
         self.upper = np.array(upper)
 
     def apply(self, position, velocity):
-        new_position = np.minimum(
-            np.maximum(position, self.lower),
-            self.upper
-        )
+        new_position = np.clip(position, self.lower, self.upper)
         return new_position, velocity
-
