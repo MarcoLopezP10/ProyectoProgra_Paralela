0acc54068f73c70ba5fa0c9fd0753b7bcfcdede8
MarcoLopezP10 <167968505+MarcoLopezP10@users.noreply.github.com>
2026-02-12 12:42:19 +0100

    Fix formatting and spacing in topology.py

--- core/topology.py
+++ core/topology.py
@@ -1,11 +1,12 @@
-from abc import ABC,abstractmethod
+from abc import ABC, abstractmethod
+
 
 class Topology(ABC):
     @abstractmethod
-    def get_best_position(self,particle,swarm):
+    def get_best_position(self, particle, swarm):
         pass
+
 
 class GlobalBestTopology(Topology):
     def get_best_position(self, particle, swarm):
         return swarm.global_best_position
-        
